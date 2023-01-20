fd34d83a6cde505e3128d7af31fce73c099ea754
antoninhrlt <antonherault@gmail.com>
2023-01-20 08:34:57 +0100

    Init project (flutter)

---
+++ lib/sherlock.dart
@@ -0,0 +1,5 @@
+library sherlock;
+
+bool helloSherlock() {
+  return true;
+}
---
+++ test/sherlock_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:sherlock/sherlock.dart';
+
+void main() {
+  test('hello', () {
+    helloSherlock();
+  });
+}
