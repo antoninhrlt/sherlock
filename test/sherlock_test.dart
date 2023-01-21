2be1c4ca6092d11cca5fbcdcb20d25188d4f14b6
antoninhrlt <antonherault@gmail.com>
2023-01-21 12:40:26 +0100

    Tests for the new query functions

--- test/sherlock_test.dart
+++ test/sherlock_test.dart
@@ -2,35 +2,60 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sherlock/sherlock.dart';
 
+final activities = [
+  {
+    'title': 'Surf',
+    'description': 'Live the wave',
+    'id': 1,
+  },
+  {
+    'title': 'LiveStream',
+    'description': 'Watch live streams',
+    'id': 2,
+  },
+  {
+    'title': 'Extreme VR',
+    'description': 'VR immersion',
+    'id': 3,
+  },
+  {
+    'title': 'Extreme VR 2',
+    'description': 'New VR immersion',
+    'id': 4,
+  },
+  {
+    'title': 'Meet cats',
+    'description': 'Cool thing to do',
+    'id': 5,
+  },
+  {
+    'title': 'Meet dogs',
+    'description': 'Don\'t you want to meet cats ?',
+    'id': 6,
+  },
+  {
+    'title': 'Parc',
+    'openingHours': {
+      'monday': [7, 17],
+      'tuesday': [7, 18],
+    },
+    'id': 7,
+  },
+  {
+    'title': 'Parc',
+    'openingHours': {
+      'tuesday': [5, 22],
+    },
+    'id': 8,
+  }
+];
+
 void main() {
   test('hello', () {
     assert(helloSherlock());
   });
 
-  test('seek', () {
-    final activities = [
-      {
-        'title': 'Surf',
-        'description': 'Live the wave',
-        'id': 1,
-      },
-      {
-        'title': 'LiveStream',
-        'description': 'Watch live streams',
-        'id': 4,
-      },
-      {
-        'title': 'Extreme VR',
-        'description': 'VR immersion',
-        'id': 2,
-      },
-      {
-        'title': 'Extreme VR 2',
-        'description': 'New VR immersion',
-        'id': 3,
-      }
-    ];
-
+  test('query', () {
     final sherlock = Sherlock(elements: activities);
 
     /// All activities where their title contains the word 'vr'.
@@ -45,8 +70,36 @@ void main() {
 
     sherlock.forget();
 
+    /// All activities where their description contains the word 'live'.
+    sherlock.query(where: 'description', regex: r'(live)');
+    debugPrint(sherlock.results.toString());
+
+    sherlock.forget();
+
+    /// All activities where their description or title contains the word 'cat'.
+    sherlock.query(where: 'title', regex: r'cat');
+    sherlock.query(where: 'description', regex: r'cat');
+
+    debugPrint(sherlock.results.toString());
+
+    sherlock.forget();
+
     /// All activities where at least one column's value contains the word 'vr'.
-    //sherlock.query(where: '*', what: 'vr');
-    //debugPrint(sherlock.results.toString());
+    sherlock.query(where: '*', regex: r'vr');
+    debugPrint(sherlock.results.toString());
+
+    sherlock.forget();
+
+    /// All activities where are monday is specified in the opening hours.
+    sherlock.queryExists(where: 'openingHours', what: 'monday');
+    debugPrint(sherlock.results.toString());
+
+    sherlock.forget();
+
+    /// All activities having a title corresponding to 'Parc'.
+    sherlock.queryBool(where: 'title', fn: (value) => value == 'Parc');
+    debugPrint(sherlock.results.toString());
+
+    sherlock.forget();
   });
 }
