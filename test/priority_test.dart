0fe42385c80895785a96d42cb79b49b8155481b1
antoninhrlt <antonherault@gmail.com>
2023-01-23 21:25:33 +0100

    Change the import for the priority map type

--- test/priority_test.dart
+++ test/priority_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sherlock/priority.dart';
+import 'package:sherlock/types.dart';
 import 'package:sherlock/sherlock.dart';
 
 void main() {
