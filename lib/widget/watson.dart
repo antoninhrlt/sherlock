47b80d0d08c53541979ec7f6aa1d6a3746174dc3
antoninhrlt <antonherault@gmail.com>
2023-03-19 13:07:40 +0100

    Create a search bar widget

---
+++ lib/widget/watson.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class WatsonSearchBar extends StatefulWidget {
+  const WatsonSearchBar({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<WatsonSearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("test");
+  }
+}
