9e90e03a52d6759014e1a040ecdecc6e687b9742
antoninhrlt <antonherault@gmail.com>
2023-05-13 19:40:55 +0200

    Add test for users smart search

---
+++ test/find_users_test.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sherlock/sherlock.dart';
+
+void main() {
+  final users = [
+    {
+      'firstName': 'Finn',
+      'lastName': 'Thornton',
+      'city': 'Edinburgh',
+      'id': 1,
+    },
+    {
+      'firstName': 'Suz',
+      'lastName': 'Judy',
+      'city': 'Paris',
+      'id': 2,
+    },
+    {
+      'firstName': 'Suz',
+      'lastName': 'Crystal',
+      'city': 'Edinburgh',
+      'id': 3,
+    },
+  ];
+
+  test('findUsers', () {
+    final sherlock = Sherlock(elements: users);
+    sherlock.search(where: ['firstName'], input: 'f');
+    debugPrint('results: ${sherlock.results}');
+  });
+}
