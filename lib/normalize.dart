7f1669430c57f4d5ae3b545208f740b620222c0e
antoninhrlt <antonherault@gmail.com>
2023-05-25 17:39:03 +0200

    Review the normalization settings class

--- lib/normalize.dart
+++ lib/normalize.dart
@@ -1,143 +1,90 @@
 import 'package:diacritic/diacritic.dart' as diacritic;
 
-/// Settings about string normalization.
+/// Settings for strings normalization.
+///
+/// Mainly used by the [Normalize] extension for [String]s.
 class NormalizationSettings {
-  /// The actual settings being a map with the settings id and their boolean
-  /// value to known if this type of normalization has to be done or not.
-  final Map<String, bool?> settings;
+  /// Whether the case must be normalized (to lower case). It means transforming all uppercased characters to lowercased
+  /// characters.
+  final bool normalizeCase;
 
-  /// Values `true` are the settings that will be applied to the strings.
-  /// For example, if [normalizeCase] is `true`, it will be case-insensitive.
-  ///
-  /// Setting [normalizeCase] is used to transform any uppercased string into a
-  /// lowercased string.
-  ///
-  /// Setting [normalizeCaseType] is used to transform any camelCased string or
-  /// snake_cased string into a normal string, without case type.
-  /// See [Normalize.normalizeCaseType] to know more about it.
-  ///
-  /// Setting [removeDiacritics] is used to remove the diacritics (accents and
-  /// other additional symbol on the characters).
-  NormalizationSettings({
-    bool? normalizeCase,
-    bool? normalizeCaseType,
-    bool? removeDiacritics,
-  }) : settings = {'case': normalizeCase, 'caseType': normalizeCaseType, 'diacritics': removeDiacritics};
+  /// Whether the case type must be normalized (to a classic case). It means transform *camelCase* or *snake_case* to
+  /// *normal case*.
+  final bool normalizeCaseType;
 
-  /// The defaults settings for normalizing.
-  /// ```dart
-  /// NormalizeSettings(
-  ///     normalizeCase: true,
-  ///     normalizeCaseType: false,
-  ///     removeDiacritics: true,
-  /// );
-  /// ```
+  /// Whether the diacritics must be removed of the strings. It means removing all the accents and other added symbols
+  /// from the letters.
+  final bool removeDiacritics;
+
+  /// Creates settings for strings normalization.
+  ///
+  /// If a setting is not specified, it will be false by default.
+  const NormalizationSettings({
+    this.normalizeCase = false,
+    this.normalizeCaseType = false,
+    this.removeDiacritics = false,
+  });
+
+  /// The defaults settings for normalizing strings
+  ///
+  /// - The case is normalized
+  /// - The case type is not normalized
+  /// - The diacritics are removed
   const NormalizationSettings.defaults()
-      : settings = const {
-          'case': true,
-          'caseType': false,
-          'diacritics': true,
-        };
+      : normalizeCase = true,
+        normalizeCaseType = false,
+        removeDiacritics = true;
 
-  /// Updates the settings from another [NormalizationSettings] object [source].
+  /// The defaults settings to normalize nothing. Can be used to test matching strings.
   ///
-  /// Null elements of [source] stands for not changing the values.
-  void updateFrom(NormalizationSettings source) {
-    for (var id in ['case', 'caseType', 'diacritics']) {
-      // Does not change because the given source's value is null.
-      if (source[id] == null) {
-        continue;
-      }
-
-      // Updates setting from [source].
-      settings[id] = source[id];
-    }
-  }
-
-  /// The defaults normalizing settings for a perfect match between strings.
-  /// ```dart
-  /// NormalizeSettings(
-  ///     normalizeCase: false,
-  ///     normalizeCaseType: false,
-  ///     removeDiacritics: false,
-  /// );
-  /// ```
-  NormalizationSettings.matching()
-      : settings = {
-          'case': false,
-          'caseType': false,
-          'diacritics': false,
-        };
+  /// - The case is normalized
+  /// - The case type is not normalized
+  /// - The diacritics are removed
+  const NormalizationSettings.matching()
+      : normalizeCase = false,
+        normalizeCaseType = false,
+        removeDiacritics = false;
 
   /// Creates a new [NormalizationSettings] object same as [object].
   NormalizationSettings.from(NormalizationSettings object)
-      : settings = {
-          'case': object['case'],
-          'caseType': object['caseType'],
-          'diacritics': object['diacritics'],
-        };
-
-  /// Changes the case sensitivity.
-  ///
-  /// If [caseSensitive] is `true`, the case setting will be set on `false`.
-  /// If [caseSensitive] is `false`, the case setting will be set on `true`.
-  set caseSensitivity(bool caseSensitivity) {
-    settings['case'] = !caseSensitivity;
-  }
-
-  bool get caseSensitivity => !(this['case'] ?? false);
-
-  /// Gets a setting from [settings] from its [id].
-  ///
-  /// Throws an error when the [id] is not corresponding to a valid setting.
-  bool? operator [](String id) {
-    if (!settings.containsKey(id)) {
-      throw '$id is not a valid setting';
-    }
-
-    return settings[id];
-  }
-
-  /// Returns [settings] into [String].
-  @override
-  String toString() {
-    return settings.toString();
-  }
+      : normalizeCase = object.normalizeCase,
+        normalizeCaseType = object.normalizeCaseType,
+        removeDiacritics = object.removeDiacritics;
 }
 
-/// Extends [String] with a normalizing function.
+/// Extends [String] with a function to normalize it.
 extension Normalize on String {
-  /// Normalizes a string following the given [settings].
+  /// Normalizes a string following the given normalization [settings].
   String normalize(NormalizationSettings settings) {
     String normalized = this;
 
     // Normalizes the case type, but keep the case (upper or lower)
-    if (settings['caseType'] ?? false) {
+    if (settings.normalizeCaseType) {
       normalized = normalized.normalizeCaseType();
     }
 
     // Lowercases the string.
-    if (settings['case'] ?? false) {
+    if (settings.normalizeCase) {
       normalized = normalized.toLowerCase();
     }
 
     // Removes the diacritics.
-    if (settings['diacritics'] ?? false) {
+    if (settings.removeDiacritics) {
       normalized = normalized.removeDiacritics();
     }
 
     return normalized;
   }
 
-  /// Normalizes the case whether it is snake case or camel case. Conserves the
+  /// Normalizes the case whether it is *snake_case* or *camelCase*. Conserves the
   /// case (upper or lower).
   ///
-  /// Examples :
-  ///   fooBar -> foo bar
-  ///   FooBar -> Foo bar
-  ///   foo_bar -> foo bar
-  ///   fooBar_baz -> foo bar baz
-  ///   FooBar_baz -> Foo bar baz
+  /// ## Examples
+  /// - fooBar -> foo bar
+  /// - FooBar -> Foo bar
+  /// - foo_bar -> foo bar
+  /// - fooBar_baz -> foo bar baz
+  /// - FooBar_baz -> Foo bar baz
   String normalizeCaseType() {
     String normalized = this;
 
@@ -150,8 +97,10 @@ extension Normalize on String {
   /// Removes the diacritics of a string thanks to the [diacritic] package.
   String removeDiacritics() => diacritic.removeDiacritics(this);
 
-  /// Returns the string being a camel-cased string changed into a normal case.
-  /// Example : `'helloWorld'` -> `'hello world'`.
+  /// Transforms a string in camel case to normal case.
+  ///
+  /// ## Example
+  /// `'helloWorld'` -> `'hello world'`.
   String normalizeCamelCase() {
     String normalized = this;
 
@@ -186,8 +135,10 @@ extension Normalize on String {
     return normalized;
   }
 
-  /// Returns the string being a snake-cased string changed into a normal case.
-  /// Example : `'hello_world'` -> `'hello world'`.
+  /// Transforms a string in snake case to normal case.
+  ///
+  /// ## Example
+  /// `'hello_world'` -> `'hello world'`.
   String normalizeSnakeCase() {
     String normalized = this;
 
