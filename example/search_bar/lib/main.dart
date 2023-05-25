d9c3d4de9656fe7268446e5ac0af09b6f88f96a9
antoninhrlt <antonherault@gmail.com>
2023-05-25 19:44:34 +0200

    Make a better presentation example

--- example/search_bar/lib/main.dart
+++ example/search_bar/lib/main.dart
@@ -28,18 +28,28 @@ class MyApp extends StatelessWidget {
 class ExampleView extends StatefulWidget {
   ExampleView({super.key});
 
-  final List<Map<String, dynamic>> users = [
+  final List<Map<String, dynamic>> characters = [
     {
-      'name': 'Finn Thornton',
-      'city': 'Edinburgh',
+      'name': 'Jiji',
+      'description': 'Black cat with large white eyes and black pupils',
+      'from': 'Kiki\'s delivery service',
+      'by': 'Studio Ghibli',
+      'image': 'https://i.pinimg.com/originals/10/f5/b8/10f5b8f9a07cbebd069fe90e0c8417c9.jpg',
     },
     {
-      'name': 'Suz Judy',
-      'city': 'Paris',
+      'name': 'Haku',
+      'description': 'He has straight, dark green hair in a bob haircut and slanted, green eyes',
+      'from': 'Spirited Away',
+      'by': 'Studio Ghibli',
+      'image': 'https://i.pinimg.com/736x/21/72/ff/2172ff97bf332adb0c2c003fa2746688.jpg',
     },
     {
-      'name': 'Suz Crystal',
-      'city': 'Edinburgh',
+      'name': 'Soot Sprites',
+      'description': 'They are small, round balls made from the soot that dwells in old and abandoned houses',
+      'from': 'My Neighbor Totoro',
+      'by': 'Studio Ghibli',
+      'image':
+          'https://media0.giphy.com/media/oje6kPRIef6Gk/200.gif?cid=6c09b952152c6g4ft5pmn8bgv6knnhnx8mqvac8mu37xmzlx&ep=v1_gifs_search&rid=200.gif&ct=g',
     },
   ];
 
@@ -48,7 +58,7 @@ class ExampleView extends StatefulWidget {
 }
 
 class ExampleState extends State<ExampleView> {
-  List<Map<String, dynamic>> results = [];
+  List<Map<String, dynamic>> _results = [];
 
   @override
   Widget build(BuildContext context) {
@@ -56,17 +66,48 @@ class ExampleState extends State<ExampleView> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SherlockSearchBar(
-              sherlock: Sherlock(elements: widget.users),
-              sherlockCompletion: SherlockCompletion(where: 'name', elements: widget.users),
+              isFullScreen: true,
+              sherlock: Sherlock(elements: widget.characters),
+              sherlockCompletion: SherlockCompletion(where: 'by', elements: widget.characters),
+              sherlockCompletionMinResults: 1,
               onSearch: (input, sherlock) {
                 setState(() {
-                  results = sherlock.search(input: input).sorted().unwrap();
+                  _results = sherlock.search(input: input).sorted().unwrap();
                 });
               },
+              completionsBuilder: (context, completions) => SherlockCompletionsBuilder(
+                completions: completions,
+                buildCompletion: (completion) => Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Row(
+                    children: [
+                      Text(
+                        completion,
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                      const Spacer(),
+                      const Icon(Icons.check),
+                      const Icon(Icons.close)
+                    ],
+                  ),
+                ),
+              ),
             ),
-            ...results.map((e) => UserCard(user: e)),
+            if (_results.isNotEmpty)
+              const Padding(
+                padding: EdgeInsets.fromLTRB(8, 8, 0, 0),
+                child: Text(
+                  'Results',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+            ..._results.map((e) => CharacterCard(character: e)),
           ],
         ),
       ),
@@ -74,35 +115,76 @@ class ExampleState extends State<ExampleView> {
   }
 }
 
-class UserCard extends StatelessWidget {
-  final Map<String, dynamic> user;
+class CharacterCard extends StatelessWidget {
+  final Map<String, dynamic> character;
 
-  const UserCard({
+  const CharacterCard({
     super.key,
-    required this.user,
+    required this.character,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8),
-      child: Row(
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(0, 12, 12, 0),
-            child: Icon(
-              Icons.person,
-              size: 52,
+      child: IntrinsicHeight(
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 12, 0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(6.0),
+                child: Image.network(
+                  character['image'],
+                  width: 65,
+                  height: 65,
+                ),
+              ),
             ),
-          ),
-          Text(
-            user['name'],
-            style: const TextStyle(fontWeight: FontWeight.bold),
-          ),
-          const Spacer(),
-          const Icon(Icons.location_city),
-          Text(user['city']),
-        ],
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        character['name'],
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        ' by ${character['by']}',
+                        style: const TextStyle(fontSize: 13),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    character['description'],
+                    overflow: TextOverflow.clip,
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(3, 0, 8, 0),
+              child: VerticalDivider(
+                color: Colors.black,
+                thickness: 0.2,
+                width: 1,
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.movie),
+                Text(
+                  character['from'],
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
